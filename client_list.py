9ed60e7059e00877f78d1471dc94b8bcb894da5a
adam <adam.lacaria@gmail.com>
2021-02-06 21:38:54 -0500

    added comments

--- client_list.py
+++ client_list.py
@@ -8,17 +8,24 @@ from googleapiclient import sample_tools
 from oauth2client.client import AccessTokenRefreshError
 
 
-def main(argv):
-    service, flags = sample_tools.init(argv, 'analytics', 'v3', __doc__, __file__, scope='https://www.googleapis.com/auth/analytics.readonly')
-    dataframe = get_first_profile_id(service)
-    
 
-def get_first_profile_id(service):
+def main(argv):
+    #calling google api service
+    service, flags = sample_tools.init(argv, 'analytics', 'v3', __doc__, __file__, scope='https://www.googleapis.com/auth/analytics.readonly')
+    
+    #return dataframe unused 
+    dataframe = get_ga_accounts(service)
+    
+#gathers all web properties under analytics access, merges accountname and writes to csv
+def get_ga_accounts(service):
 
   prop_data =[]
   account_data= []
+
+  #dataFrame unique record for each ga web property 
   df = pd.DataFrame(prop_data, columns =['AccountID','PropertyID', 'PropertyName','PropertyProfileCount','PropertyVertical','PropertyInternalID','PropertyLevel','WebsiteURL','Created','Updated'])
 
+  #properties object via service, collecting all accounts under analytics
   properties = service.management().webproperties().list(
     accountId='~all').execute()
   
@@ -57,27 +64,37 @@ def get_first_profile_id(service):
       updated = property.get('updated')
       print ('Updated ' + str(updated))
 
+      #extend, adding multiple variables to the list
       internal_data.extend((accountid,propid,propname,profilec,vertical,internalid,proplevel,url,created,updated))
       to_append = internal_data
       df_length = len(df)
+
+      #unique web property added to end of dataFrame
       df.loc[df_length] = to_append
        
-
+  
   df2 = pd.DataFrame(account_data,columns =['AccountID','AccountName'])
 
+ 
   accounts = service.management().accounts().list().execute()
   for account in accounts.get('items',[] ):
     internal_account = []
     account_name = account.get('name')
     account_id = account.get('id')
+    
+    #unique account added to list 
     internal_account.extend((account_id,account_name))
 
     to_append2 = internal_account
     df2_length = len(df2)
+
+    #unique account added to end of account dataFrame
     df2.loc[df2_length] = to_append2
 
-
+  #vlookup to match accountName from df2, properties object did not have accountName just accountID
   left_join = pd.merge(df,df2,on ='AccountID',how = 'left')
+  
+  #writing to csv from dataFrame method 'to_csv'
   left_join.to_csv(r'C:\Users\Adam LaCaria\Desktop\google_api\client_list.csv', index=False,header=True)
   return left_join
   
